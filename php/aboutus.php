a1683f148d65c2e845d99c27d63666a576077aea
Andree febaraaf <andre_ajib@yahoo.com>
2014-01-18 16:09:04 +0700

    new file

---
+++ php/aboutus.php
@@ -0,0 +1,39 @@
+<?php session_start();  ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us</title>
+<link href="../css/kost.css" rel="stylesheet" type="text/css" />
+<link href="../css/css.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+
+<body>
+ <!-- Begin Wrapper -->
+   <div id="wrapper">
+   
+         <!-- Begin Header -->
+			<?php 
+				include("header.php");
+            ?>         
+		 <!-- End Header -->
+		 
+         <!-- Begin Faux Columns -->
+		 <div id="faux">
+		 
+		       <!-- Begin Left Column -->
+		       <div><br /><br />
+				<b><a style="font-size:36px;">Kost Online</a></b>
+                <b><p style="font-size:14px">Sebuah website yang menyediakan info-info kost di daerah kampus Mikroskil medan. Tidak hanya menyediakan info kost saja, tetapi bisa juga langsung memesannya</p></b>
+                <div id="test">
+                </div>
+               </div>
+               
+           </div>
+           <?php include("footer.php") ?>
+</div>
+</body>
+
+</html>
