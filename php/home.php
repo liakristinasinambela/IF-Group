0043054a4d4e6fdea6bda39099d22b84e2fe60f6
andreefebaraaf <andreefebaraaf@gmail.com>
2014-01-18 16:29:27 +0700

    edit home.php

--- php/home.php
+++ php/home.php
@@ -4,20 +4,25 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Home</title>
-<link href="kost.css" rel="stylesheet" type="text/css" />
-<link href="css.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript">
-<!--
+<link href="../css/kost.css" rel="stylesheet" type="text/css" />
+<link href="../css/css.css" rel="stylesheet" type="text/css" />
 
-	
 
-//-->
-</script>
 </head>
 
 
 <body>
- <!-- Begin Wrapper -->
+<script type="text/javascript" src="../jquery/jquery-1.3.2.js"></script>
+        <script type="text/javascript">
+		$(function() {
+			<?php	if(isset($_SESSION['user_name'] )){?>
+			      $('#rightcolumn1').hide();
+				<?php  }else{?>
+				  $('#rightcolumn1').show();
+				  <?php } ?>
+				  
+		});
+</script> <!-- Begin Wrapper -->
    <div id="wrapper">
    
          <!-- Begin Header -->
@@ -31,12 +36,14 @@
 		 
 		       <!-- Begin Left Column -->
 		       <div id="leftcolumn"><br /><br />
-               <h1><b>KOST Online</b></h1><br /><br />
-               <h3>Tempat pencarian Kost secara Online, tidak perlu lagi mencari secara manual</h3>
+               <a style="font-size:46px;">Kost Online</a><br /><br />
+               <b><p style="font-size:18px">Tempat pencarian Kost secara Online, tidak perlu lagi mencari secara manual</p></b>
+               <div id="test"></div>
                </div>
                <div id="rightcolumn1">
                <form method="post" action="daftar.php" name="form">
-					<span><input name="Nama_Depan" type="text" placeholder="Nama Depan" class="home"/>
+					<span><input id="form1" name="Nama_Depan" type="text" placeholder="Nama Depan" class="home"/>
+                    <input id="form1" name="Nama_Tengah" type="text" placeholder="Nama Tengah" class="home"/>
                     <input name="Nama_Belakang" type="text" placeholder="Nama Belakang" class="home"/></span>
                 	<input name="User_Name" type="text" placeholder="User Name anda"/>
                     <input type="text" name="email" placeholder="Email anda" />
@@ -47,5 +54,7 @@
                 </div>
            </div>
            <?php include("footer.php") ?>
+</div>
 </body>
+
 </html>
