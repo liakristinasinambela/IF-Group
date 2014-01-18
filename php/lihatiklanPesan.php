dbe636121f999a6bb820c1c3af00e2af8a3fef94
muhammadhasansinaga <muhammadhasansinaga@yahoo.co.id>
2014-01-18 16:23:46 +0700

    update lihatiklanPesan.php

--- php/lihatiklanPesan.php
+++ php/lihatiklanPesan.php
@@ -5,7 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Buat Iklan</title>
+<title>Pesan Iklan</title>
 
 </head>
 
@@ -14,11 +14,9 @@
    <!-- Begin Wrapper -->
    <div id="wrapper">
     <?php $p =getLihatiklan($_GET['id']);
-		echo $p['Alamat_Kost'];
 		?>
-    
-    <?php echo $p['Alamat_Kost'];?>
          <!-- Begin Header -->
+         
          <?php include("header.php"); ?>
 		 <!-- End Header -->
 		 
@@ -29,13 +27,14 @@
 		       <div id="leftcolumn">
                     <form method="post" action="pesan.php" enctype="multipart/form-data" >
                         <a><b>Pemesanan Kamar:</b></a><br />
+                        <input name="username" type="hidden" value="<?php echo $_SESSION['user_name'];?>" />
                         <input name="Nama_Kost" type="text" value="<?php echo $p['Nama_Kost'];?>" readonly="readonly"/><br />
                         <textarea name="Alamat_Kost" readonly="readonly"><?php echo $p['Alamat_Kost'];?></textarea>
                         <br />
                         <input name="Jenis_Kamar" type="text" value="<?php echo $p['Jenis_Kamar'];?>" readonly="readonly"/>
-                        <input type="text" name="tarif" value="<?php echo $p['tarif'];?>" readonly="readonly"/>
-                        <input type="text" name="qty" value="<?php echo $p['qty'];?>" readonly="readonly"/>
-                        <textarea name="fasilitas"placeholder="Fasilitas Kamar"></textarea><br />
+                        <input type="text" name="tarif" value="<?php echo $p['Tarif_Kamar'];?>" readonly="readonly"/>
+                        <input type="text" name="qty" value="<?php echo $p['Banyak_Kamar'];?>" readonly="readonly"/>
+                        <textarea name="fasilitas" readonly="readonly"><?php echo $p['Fasilitas_Kamar'];?></textarea><br />
                        <input type="submit" value="Pesan" />
                      
                 </div>
@@ -43,9 +42,14 @@
 		 
 		       <!-- Begin Content Column -->
 		       <div id="content">
-                   <label for="kost">Gambar Kost : </label><br /><br />
+                   <label for="kost">Gambar Kost : </label><br />
+                   <div >
+                   <img width="200px"height="200px" src="<?php echo $p['Gambar_Kost']; ?>">
+                   </div>
                    <label for="kamar">Upload Gambar Kamar : </label><br />
-             		 
+             		<div>
+                    <img width="200px"height="200px" src="<?php echo $p['Gambar_kamar']; ?>">
+                    </div>
                     </form>
 		       </div>
 		       <!-- End Content Column -->
